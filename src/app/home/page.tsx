80a9147a240ff41e2a8626706e5ee6cff71014f0
felipethecreator <feliperodriguesqueiroz122@gmail.com>
2025-04-30 16:40:25 -0300

    feat: Home page added

---
+++ src/app/home/page.tsx
@@ -0,0 +1,11 @@
+import Header from '../../components/Header'
+import Hero from '../../components/Hero'
+
+export default function MainPage() {
+    return(
+        <div className='h-screen bg-[linear-gradient(180deg,#0A0C15_16%,#141E30,#243B55)]'>
+            <Header/>
+            <Hero/>
+        </div>
+    )
+}
